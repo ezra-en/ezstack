13e0f9bd16e3c6bcb1b202ab26dc7141a73452b1
EzraEn <7076802+ezra-en@users.noreply.github.com>
2025-08-31 01:43:25 +0800

    feat: toggle feature

--- convex/tasks.ts
+++ convex/tasks.ts
@@ -1,8 +1,24 @@
-import { query } from "./_generated/server";
+import { v } from "convex/values";
+import { mutation, query } from "./_generated/server";
 
-export const get = query({
+export const getAllTasks = query({
   args: {},
   handler: async (ctx) => {
     return await ctx.db.query("tasks").collect();
+  },
+});
+
+export const toggleTask = mutation({
+  args: {
+    id: v.id("tasks"),
+    isCompleted: v.boolean(),
+  },
+  handler: async (ctx, args) => {
+    const { id, isCompleted } = args;
+    await ctx.db
+      .patch(id, {
+        isCompleted,
+      });
+    console.log(await ctx.db.get(id));
   },
 });
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,27 @@
 "use client";
 
 import Image from "next/image";
-import { useQuery } from "convex/react";
+import { useMutation, useQuery } from "convex/react";
 import { api } from "../../convex/_generated/api";
 import { Card } from "../components/ui/card";
 import { Checkbox } from "../components/ui/checkbox";
 
 export default function Home() {
-  const tasks = useQuery(api.tasks.get);
+  const tasks = useQuery(api.tasks.getAllTasks);
+  const toggleTask = useMutation(api.tasks.toggleTask);
+
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
-      {tasks?.map(({ _id, text }) => (
+      {tasks?.map(({ _id, text, isCompleted }) => (
         <Card
           key={_id}
           className="w-full max-w-md mb-4 p-4 flex flex-row items-center gap-4"
         >
-          <Checkbox id={`checkbox-${_id}`} />
+          <Checkbox
+            id={`checkbox-${_id}`}
+            checked={isCompleted}
+            onCheckedChange={async (checked) => await toggleTask({ id: _id, isCompleted: checked === true })}
+          />
           <label
             htmlFor={`checkbox-${_id}`}
             className="cursor-pointer select-none"
