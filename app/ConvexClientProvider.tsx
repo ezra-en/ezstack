688fc31758c0c14db131f7c8c264396d96419671
EzraEn <7076802+ezra-en@users.noreply.github.com>
2025-11-14 15:29:16 +0800

    add @convex/better-auth

--- app/ConvexClientProvider.tsx
+++ app/ConvexClientProvider.tsx
@@ -1,6 +1,8 @@
 "use client";
 
-import { ConvexProvider, ConvexReactClient } from "convex/react";
+import { authClient } from "@/lib/auth-client";
+import { ConvexBetterAuthProvider } from "@convex-dev/better-auth/react";
+import { ConvexReactClient } from "convex/react";
 import type { ReactNode } from "react";
 
 if (!process.env.NEXT_PUBLIC_CONVEX_URL) {
@@ -9,8 +11,15 @@ if (!process.env.NEXT_PUBLIC_CONVEX_URL) {
 	);
 }
 
-const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL);
+const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL, {
+  // Optionally pause queries until the user is authenticated
+  expectAuth: true, 
+});
 
 export function ConvexClientProvider({ children }: { children: ReactNode }) {
-	return <ConvexProvider client={convex}>{children}</ConvexProvider>;
+  return (
+    <ConvexBetterAuthProvider client={convex} authClient={authClient}>
+      {children}
+    </ConvexBetterAuthProvider>
+  );
 }
---
+++ convex/auth.ts
@@ -0,0 +1,50 @@
+import { createClient, type GenericCtx } from "@convex-dev/better-auth";
+import { convex } from "@convex-dev/better-auth/plugins";
+import { components } from "./_generated/api";
+import type { DataModel } from "./_generated/dataModel";
+import { query } from "./_generated/server";
+import { betterAuth } from "better-auth";
+
+if (!process.env.NEXT_PUBLIC_DEPLOYMENT_URL) {
+	throw new Error(
+		"Missing NEXT_PUBLIC_DEPLOYMENT_URL environment variable. Please set it in your Convex dashboard.",
+	);
+}
+const siteUrl = process.env.NEXT_PUBLIC_DEPLOYMENT_URL;
+
+// The component client has methods needed for integrating Convex with Better Auth,
+// as well as helper methods for general use.
+export const authComponent = createClient<DataModel>(components.betterAuth);
+
+export const createAuth = (
+	ctx: GenericCtx<DataModel>,
+	{ optionsOnly } = { optionsOnly: false },
+) => {
+	return betterAuth({
+		// disable logging when createAuth is called just to generate options.
+		// this is not required, but there's a lot of noise in logs without it.
+		logger: {
+			disabled: optionsOnly,
+		},
+		baseURL: siteUrl,
+		database: authComponent.adapter(ctx),
+		// Configure simple, non-verified email/password to get started
+		emailAndPassword: {
+			enabled: true,
+			requireEmailVerification: false,
+		},
+		plugins: [
+			// The Convex plugin is required for Convex compatibility
+			convex(),
+		],
+	});
+};
+
+// Example function for getting the current user
+// Feel free to edit, omit, etc.
+export const getCurrentUser = query({
+	args: {},
+	handler: async (ctx) => {
+		return authComponent.getAuthUser(ctx);
+	},
+});
